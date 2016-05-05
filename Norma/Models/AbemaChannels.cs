9be21972113ed10a3a31c659575291fb9e0341cc
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-05 19:07:03 +0900

    Add channel lists.

---
+++ Norma/Models/AbemaChannels.cs
@@ -0,0 +1,115 @@
+﻿namespace Norma.Models
+{
+    internal enum AbemaChannels
+    {
+        /// <summary>
+        ///     1ch - Abema news/
+        /// </summary>
+        AbemaNews,
+
+        /// <summary>
+        ///     2ch - Abema SPECIAL
+        /// </summary>
+        AbemaSpecial,
+
+        /// <summary>
+        ///     3ch - SPECIAL PLUS
+        /// </summary>
+        SpecialPlus,
+
+        /// <summary>
+        ///     4ch - REALITY SHOW
+        /// </summary>
+        RealityShow,
+
+        /// <summary>
+        ///     5ch - MTV HITS
+        /// </summary>
+        MtvHits,
+
+        /// <summary>
+        ///     6ch - SPACE SHOWER MUSIC
+        /// </summary>
+        SpaceShowerMusic,
+
+        /// <summary>
+        ///     7ch - ドラマ CHANNEL
+        /// </summary>
+        DramaChannel,
+
+        /// <summary>
+        ///     8ch - Documentary
+        /// </summary>
+        Documentary,
+
+        /// <summary>
+        ///     9ch - バラエティ CHANNEL
+        /// </summary>
+        VarietyChannel,
+
+        /// <summary>
+        ///     10ch - ペット
+        /// </summary>
+        Pet,
+
+        /// <summary>
+        ///     11ch - CLUB CHANNEL
+        /// </summary>
+        ClubChannel,
+
+        /// <summary>
+        ///     12ch - WORLD SPORTS
+        /// </summary>
+        WorldSports,
+
+        /// <summary>
+        ///     13ch - ヨコノリ Surf Snow Skate
+        /// </summary>
+        YokonoriSports,
+
+        /// <summary>
+        ///     14ch - VICE
+        /// </summary>
+        Vice,
+
+        /// <summary>
+        ///     15ch - アニメ24
+        /// </summary>
+        Anime24,
+
+        /// <summary>
+        ///     16ch - 深夜アニメ
+        /// </summary>
+        MidnightAnime,
+
+        /// <summary>
+        ///     17ch - なつかしアニメ
+        /// </summary>
+        OldtimeAnime,
+
+        /// <summary>
+        ///     18ch - 家族アニメ
+        /// </summary>
+        FamilyAnime,
+
+        /// <summary>
+        ///     19ch - EDGE SPORT HD
+        /// </summary>
+        EdgeSportHd,
+
+        /// <summary>
+        ///     20ch - 釣り
+        /// </summary>
+        Fishing,
+
+        /// <summary>
+        ///     21ch - 麻雀
+        /// </summary>
+        Mahjong,
+
+        /// <summary>
+        ///     22ch - AbemaTV FRESH!
+        /// </summary>
+        AbemaTvFresh
+    }
+}
