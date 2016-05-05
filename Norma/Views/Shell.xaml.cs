4fea52bec7c9581aac29b361c7f8f148b73a0154
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-05 16:36:55 +0900

    Apply Prism Style.

---
+++ Norma/Views/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Norma.Views
+{
+    /// <summary>
+    ///     Shell.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
