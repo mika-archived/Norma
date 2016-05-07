2246c717e45cb20ed0da91a99b12abd08f963556
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-07 21:44:33 +0900

    Comment list's style.

---
+++ Norma/ViewModels/Controls/AbemaCommentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+using Norma.Gamma.Models;
+using Norma.ViewModels.Internal;
+
+namespace Norma.ViewModels.Controls
+{
+    internal class AbemaCommentViewModel : ViewModel
+    {
+        public ObservableCollection<Comment> Comments { get; private set; }
+
+        public AbemaCommentViewModel()
+        {
+            Comments = new ObservableCollection<Comment>();
+        }
+    }
+}
