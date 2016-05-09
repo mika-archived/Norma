e169ee4bd99ad8bb0d87c2f5843ba9b25304e7a2
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-09 16:19:40 +0900

    Channel list is scrolled by Mouse wheel.

---
+++ Norma/Behaviors/HorizontalScrollBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Norma.Behaviors
+{
+    internal class HorizontalScrollBehavior : Behavior<ScrollViewer>
+    {
+        #region Overrides of Behavior
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseWheel += AssociatedObjectOnPreviewMouseWheel;
+        }
+
+        private void AssociatedObjectOnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scrollViewer = sender as ScrollViewer;
+            if (e.Delta > 0)
+                scrollViewer?.LineLeft();
+            else
+                scrollViewer?.LineRight();
+            e.Handled = true;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseWheel -= AssociatedObjectOnPreviewMouseWheel;
+            base.OnDetaching();
+        }
+
+        #endregion
+    }
+}
