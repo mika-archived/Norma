0ab64c11f71f48827d37c2acc34e74cbdbc2322d
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-10 19:50:12 +0900

    Product Information.

---
+++ Norma/Models/ProductInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace Norma.Models
+{
+    internal static class ProductInfo
+    {
+        public static string Name => GetAssemblyInfo<AssemblyTitleAttribute>().Title;
+
+        public static string Description => GetAssemblyInfo<AssemblyDescriptionAttribute>().Description;
+
+        public static string Company => GetAssemblyInfo<AssemblyCompanyAttribute>().Company;
+
+        public static string Copyright => GetAssemblyInfo<AssemblyCopyrightAttribute>().Copyright;
+
+        public static string Version => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public static string Support => "https://github.com/fuyuno/Norma";
+
+        private static T GetAssemblyInfo<T>() where T : Attribute
+            => (T) Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(T));
+    }
+}
--- Norma/Properties/AssemblyInfo.cs
+++ Norma/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
+
 [assembly: AssemblyTitle("Norma")]
 [assembly: AssemblyDescription("Unofficial AbemaTV Desktop Client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("kokoiroworks")]
 [assembly: AssemblyProduct("Norma")]
 [assembly: AssemblyCopyright("Copyright © Mikazuki 2016")]
 [assembly: AssemblyTrademark("")]
@@ -19,6 +18,7 @@ using System.Windows;
 // ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから
 // 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
 // その型の ComVisible 属性を true に設定してください。
+
 [assembly: ComVisible(false)]
 
 //ローカライズ可能なアプリケーションのビルドを開始するには、
@@ -30,16 +30,14 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
-                                     //(リソースがページ、
-                                     //またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
+    //(リソースがページ、
+    //またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
     ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
-                                              //(リソースがページ、
-                                              //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
-)]
-
+    //(リソースがページ、
+    //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
+    )]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
@@ -48,8 +46,9 @@ using System.Windows;
 //      ビルド番号
 //      Revision
 //
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
