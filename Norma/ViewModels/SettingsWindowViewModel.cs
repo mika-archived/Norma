1021d4c070299963da4fc72976c1d4727a1ce637
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-10 19:59:03 +0900

    Options Tab

--- Norma/ViewModels/SettingsWindowViewModel.cs
+++ Norma/ViewModels/SettingsWindowViewModel.cs
@@ -1,6 +1,15 @@
 ﻿using Norma.ViewModels.Internal;
+using Norma.ViewModels.Tabs;
 
 namespace Norma.ViewModels
 {
-    internal class SettingsWindowViewModel : ViewModel {}
+    internal class SettingsWindowViewModel : ViewModel
+    {
+        public OptionsTabViewModel OptionsTabViewModel { get; }
+
+        public SettingsWindowViewModel()
+        {
+            OptionsTabViewModel = new OptionsTabViewModel();
+        }
+    }
 }
