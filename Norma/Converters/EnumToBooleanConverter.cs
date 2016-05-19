6551c88643b423db4107757c93945e9d1ace6e55
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-19 20:59:11 +0900

    When open settings window, theme has set default value.

---
+++ Norma/Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Norma.Converters
+{
+    internal class EnumToBooleanConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() == parameter as string;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var b = value as bool?;
+            if (!b.HasValue)
+                return null;
+            return b.Value ? Enum.Parse(targetType, parameter as string ?? "", true) : null;
+        }
+
+        #endregion
+    }
+}
--- Norma/ViewModels/Tabs/Options/OthersViewModel.cs
+++ Norma/ViewModels/Tabs/Options/OthersViewModel.cs
@@ -1,52 +1,21 @@
 ﻿using MetroRadiance.UI;
 
+using Norma.Extensions;
 using Norma.Models.Config;
 using Norma.ViewModels.Internal;
+
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace Norma.ViewModels.Tabs.Options
 {
     internal class OthersViewModel : ViewModel
     {
-        private readonly OthersConfig _othersConfig;
+        public ReactiveProperty<Theme.SpecifiedColor> SelectedTheme { get; private set; }
 
         public OthersViewModel(OthersConfig oc)
         {
-            _othersConfig = oc;
-            IsDarkChecked = oc.Theme == Theme.SpecifiedColor.Dark;
-            IsLightChecked = !IsDarkChecked;
+            SelectedTheme = oc.ToReactivePropertyAsSynchronized(w => w.Theme).AddTo(this);
         }
-
-        #region IsDarkChecked
-
-        private bool _isDarkChecked;
-
-        public bool IsDarkChecked
-        {
-            get { return _isDarkChecked; }
-            set
-            {
-                if (SetProperty(ref _isDarkChecked, value))
-                    _othersConfig.Theme = value ? Theme.SpecifiedColor.Dark : Theme.SpecifiedColor.Light;
-            }
-        }
-
-        #endregion
-
-        #region IsLightChecked
-
-        private bool _isLightChecked;
-
-        public bool IsLightChecked
-        {
-            get { return _isLightChecked; }
-            set
-            {
-                // いらないだろうけど。
-                if (SetProperty(ref _isLightChecked, value))
-                    _othersConfig.Theme = value ? Theme.SpecifiedColor.Light : Theme.SpecifiedColor.Dark;
-            }
-        }
-
-        #endregion
     }
 }
