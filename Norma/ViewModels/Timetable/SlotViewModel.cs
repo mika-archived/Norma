5796ba291809a900af5969e0843641b590743cce
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-22 02:39:31 +0900

    Update layout.

--- Norma/ViewModels/Timetable/SlotViewModel.cs
+++ Norma/ViewModels/Timetable/SlotViewModel.cs
@@ -12,12 +12,14 @@ namespace Norma.ViewModels.Timetable
         public string EndAt => _model.EndAt.ToString("HH:mm");
         public string Description => _model.Model.TableHighlight;
         public int Height { get; private set; }
+        public int Top { get; private set; }
 
         public SlotViewModel(Slot program)
         {
             _model = program;
             var span = _model.EndAt - _model.StartAt;
             Height = span.Hours * 60 * 3 + span.Minutes * 3;
+            Top = _model.StartAt.Hour * 60 * 3 + _model.StartAt.Minute * 3;
         }
     }
 }
