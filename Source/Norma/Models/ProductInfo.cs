cd2d65fa73bab04ec8ebcfa01ad04986002c9c48
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-31 10:57:01 +0900

    Add dependencies.

--- Source/Norma/Models/ProductInfo.cs
+++ Source/Norma/Models/ProductInfo.cs
@@ -12,9 +12,11 @@ namespace Norma.Models
     {
         public static string Name => GetAssemblyInfo<AssemblyTitleAttribute>().Title;
 
-        public static string Description => GetAssemblyInfo<AssemblyDescriptionAttribute>().Description;
+        public static Lazy<string> Description
+            => new Lazy<string>(() => GetAssemblyInfo<AssemblyDescriptionAttribute>().Description);
 
-        public static string Company => GetAssemblyInfo<AssemblyCompanyAttribute>().Company;
+        public static Lazy<string> Company
+            => new Lazy<string>(() => GetAssemblyInfo<AssemblyCompanyAttribute>().Company);
 
         public static string Copyright => GetAssemblyInfo<AssemblyCopyrightAttribute>().Copyright;
 
@@ -24,7 +26,7 @@ namespace Norma.Models
 
         public static string Support => "https://github.com/fuyuno/Norma";
 
-        public static List<Library> Libraries => new List<Library>
+        public static Lazy<List<Library>> Libraries => new Lazy<List<Library>>(() => new List<Library>
         {
             new CEF(),
             new CEFSharp(),
@@ -35,8 +37,10 @@ namespace Norma.Models
             new ReactiveProperty(),
             new RxNET(),
             new Unity(),
-            new HardcodetWpfNotifyIcon()
-        };
+            new HardcodetWpfNotifyIcon(),
+            new DesktopToast(),
+            new NotificationsExtensions()
+        });
 
         private static T GetAssemblyInfo<T>() where T : Attribute
             => (T) Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(T));
