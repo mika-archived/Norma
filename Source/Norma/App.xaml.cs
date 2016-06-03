442a46160d6a3addb08cb55979c897a2699d113d
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-03 23:35:11 +0900

    Indent

--- Source/Norma/App.xaml.cs
+++ Source/Norma/App.xaml.cs
@@ -59,7 +59,7 @@ namespace Norma
             sb.AppendLine($"==================================================");
             sb.AppendLine($" Crash Report");
             sb.AppendLine($"--------------------------------------------------");
-            sb.AppendLine($" Environment Value");
+            sb.AppendLine($"Environment Value");
             sb.AppendLine($"  {nameof(ProductInfo.Version)} = {ProductInfo.Version}");
             sb.AppendLine($"  {nameof(NormaConstants.CefCacheDir)} = {NormaConstants.CefCacheDir}");
             sb.AppendLine($"  {nameof(NormaConstants.CefCookiesDir)} = {NormaConstants.CefCookiesDir}");
@@ -67,11 +67,11 @@ namespace Norma
             sb.AppendLine($"  {nameof(NormaConstants.IsSupportedToast)} = {NormaConstants.IsSupportedToast}");
             sb.AppendLine($"  {nameof(NormaConstants.IsSupportedNewToast)} = {NormaConstants.IsSupportedNewToast}");
             sb.AppendLine();
-            sb.AppendLine($" Exception Message");
+            sb.AppendLine($"Exception Message");
             sb.AppendLine($"  {exception.Message}");
             sb.AppendLine();
-            sb.AppendLine($" Stack Trace");
-            sb.AppendLine($"  {exception.StackTrace}");
+            sb.AppendLine($"Stack Trace");
+            sb.AppendLine($"{exception.StackTrace}");
 
             using (var sw = new StreamWriter(path))
                 sw.WriteLine(sb.ToString());
