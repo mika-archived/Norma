095048e377d7e886157a44712b04eb625e2752f7
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-09 02:32:03 +0900

    Fix typo

--- Source/Norma.Eta/Properties/Resources.Designer.cs
+++ Source/Norma.Eta/Properties/Resources.Designer.cs
@@ -187,7 +187,7 @@ namespace Norma.Eta.Properties {
         }
         
         /// <summary>
-        ///   Are you sure you wanto to delete this item? に類似しているローカライズされた文字列を検索します。
+        ///   Are you sure you want to delete this item? に類似しているローカライズされた文字列を検索します。
         /// </summary>
         public static string ConfirmDelete {
             get {
