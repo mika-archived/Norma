004be2eef99ab1315205054111c28efcf737a186
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-10 22:03:22 +0900

    Bump version
    [ci skip]

--- Source/Norma.Eta/Properties/AssemblyInfo.cs
+++ Source/Norma.Eta/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
