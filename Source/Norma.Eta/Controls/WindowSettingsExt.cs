42a7b7e904baec598c17953f272ddb700e679a4e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-12 07:28:02 +0900

    Save window placement.

---
+++ Source/Norma.Eta/Controls/WindowSettingsExt.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Windows;
+
+using MetroRadiance.UI.Controls;
+
+namespace Norma.Eta.Controls
+{
+    public class WindowSettingsExt : WindowSettings
+    {
+        [UserScopedSetting]
+        public bool? UpdateRequired
+        {
+            get { return (bool?) this["UpdateRequired"]; }
+            set { this["UpdateRequired"] = value; }
+        }
+
+        public WindowSettingsExt(Window window) : base(window)
+        {
+            UpdateRequired = true;
+        }
+    }
+}
--- Source/Norma.Iota/Views/Shell.xaml.cs
+++ Source/Norma.Iota/Views/Shell.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Configuration;
 using System.Windows;
 
 using MetroRadiance.UI.Controls;
+
+using Norma.Eta.Controls;
 
 namespace Norma.Iota.Views
 {
@@ -14,6 +17,23 @@ namespace Norma.Iota.Views
         {
             InitializeComponent();
         }
+
+        #region Overrides of MetroWindow
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            WindowSettings = new WindowSettingsExt(this);
+            var value = ((WindowSettingsExt) WindowSettings).UpdateRequired;
+            if (value.HasValue && value.Value)
+                ((ApplicationSettingsBase) WindowSettings).Upgrade();
+            else if (!value.HasValue)
+                ((WindowSettingsExt) WindowSettings).UpdateRequired = false;
+            ((WindowSettingsExt) WindowSettings).Save();
+
+            base.OnSourceInitialized(e);
+        }
+
+        #endregion
 
         protected override void OnClosed(EventArgs e)
         {
