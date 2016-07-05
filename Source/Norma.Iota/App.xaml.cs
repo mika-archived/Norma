fe5fd36c9ee3825f20db9ac0311b7d73a889b37a
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-07-05 22:06:57 +0900

    Crash reporter for Iota.

--- Source/Norma.Iota/App.xaml.cs
+++ Source/Norma.Iota/App.xaml.cs
@@ -1,6 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
 
 using MetroRadiance.UI;
+
+using Norma.Eta;
 
 namespace Norma.Iota
 {
@@ -9,6 +16,8 @@ namespace Norma.Iota
     /// </summary>
     public partial class App : Application
     {
+        private bool _isHandled;
+
         #region Overrides of Application
 
         protected override void OnStartup(StartupEventArgs e)
@@ -17,10 +26,70 @@ namespace Norma.Iota
 
             ThemeService.Current.Register(this, Theme.Dark, Accent.Blue);
 
+#if !DEBUG
+            DispatcherUnhandledException += DispatcherOnUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+#endif
+
             var bootstrap = new Bootstrapper();
             bootstrap.Run();
         }
 
         #endregion
+
+        // UI スレッド
+        // ReSharper disable once UnusedMember.Local
+        private void DispatcherOnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (!_isHandled)
+                OnUnhandledException(e.Exception);
+        }
+
+        // 他
+        // ReSharper disable once UnusedMember.Local
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (!_isHandled)
+                OnUnhandledException((Exception) e.ExceptionObject);
+        }
+
+        private void OnUnhandledException(Exception exception)
+        {
+            _isHandled = true;
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            // TODO: ApplicationInsights とかで。
+            var path = Path.Combine(NormaConstants.CrashReportsDir,
+                                    $"{DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss")}.log");
+            var sb = new StringBuilder();
+            sb.AppendLine($"==================================================");
+            sb.AppendLine($" Crash Report");
+            sb.AppendLine($"--------------------------------------------------");
+            sb.AppendLine($"Environment Value");
+            sb.AppendLine($"  Version = {version}");
+            sb.AppendLine($"  {nameof(NormaConstants.CefCacheDir)} = {NormaConstants.CefCacheDir}");
+            sb.AppendLine($"  {nameof(NormaConstants.CefCookiesDir)} = {NormaConstants.CefCookiesDir}");
+            sb.AppendLine($"  {nameof(NormaConstants.CrashReportsDir)} = {NormaConstants.CrashReportsDir}");
+            sb.AppendLine($"  {nameof(NormaConstants.IsSupportedToast)} = {NormaConstants.IsSupportedToast}");
+            sb.AppendLine($"  {nameof(NormaConstants.IsSupportedNewToast)} = {NormaConstants.IsSupportedNewToast}");
+            sb.AppendLine();
+            sb.AppendLine($"Exception Message");
+            sb.AppendLine($"  {exception.Message}");
+            sb.AppendLine();
+            sb.AppendLine($"Stack Trace");
+            sb.AppendLine($"{exception.StackTrace}");
+
+            using (var sw = new StreamWriter(path))
+                sw.WriteLine(sb.ToString());
+
+            sb.Clear();
+            sb.AppendLine(Eta.Properties.Resources.ApplicationHasBeenCrash);
+            sb.AppendLine(Eta.Properties.Resources.PleaseSendCrashReportToMe);
+            sb.AppendLine(string.Format(Eta.Properties.Resources.CrashReportPath, path));
+
+            MessageBox.Show(sb.ToString(), "Norma.Iota Crash Report", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            Current.Shutdown();
+        }
     }
 }
