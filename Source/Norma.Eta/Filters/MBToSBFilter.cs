5084024d7af2b55a9f6217b022ae00a3c3d8dbd1
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-08-02 14:14:20 +0900

    MB to SB Filter

--- Source/Norma.Eta/Filters/MBToSBFilter.cs
+++ Source/Norma.Eta/Filters/MBToSBFilter.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.VisualBasic;
+﻿using System.Text.RegularExpressions;
+
+using Microsoft.VisualBasic;
 
 namespace Norma.Eta.Filters
 {
     // ReSharper disable once InconsistentNaming
     internal class MBToSBFilter : IFilter
     {
+        private readonly Regex _regex = new Regex(@"[\uFF01-\uFF5E]+", RegexOptions.Compiled);
+
         #region Implementation of IFilter
 
         /// <summary>
-        ///     全角文字を半角文字へ正規化します。
+        ///     全角文字を半角文字へ変換します。
+        ///     対象は英数記号のみです。
         /// </summary>
         /// <param name="str">e.g. "なもり／コミック百合姫にて連載中（一迅社刊）"</param>
         /// <returns>e.g. "なもり/コミック百合姫にて連載中(一迅社刊)"</returns>
         public string Call(string str)
         {
-            return Strings.StrConv(str, VbStrConv.Narrow, 0x0411);
+            return _regex.Replace(str, w => Strings.StrConv(w.Value, VbStrConv.Narrow, 0x0411));
         }
 
         #endregion
